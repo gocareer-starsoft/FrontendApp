eb2dedc17f8a8c36a2ea38d1f2895c860a5259af
Jack Arana <arack.dev@gmail.com>
2023-09-18 12:38:25 -0500

    feat(public): updated the not-found page

--- src/app/public/pages/not-found/not-found.component.ts
+++ src/app/public/pages/not-found/not-found.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+  invalidUrl: string = "";
 
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
+
+  ngOnInit() {
+    this.invalidUrl = this.route.snapshot.url[0].path;
+  }
 }
