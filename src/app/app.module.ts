38634530a37e412596eaa003db504b9ce60d3cc3
Jack Arana <arack.dev@gmail.com>
2023-09-18 12:42:26 -0500

    feat(app-routing): added routes for landing page and not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatCardModule} from "@angular/material/card";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {HttpClientModule} from "@angular/common/http";
+
+import { MaterialModule } from "./shared/material.module";
+import { HttpClientModule } from "@angular/common/http";
 import { HeaderComponent } from './public/components/header/header.component';
 import { FooterComponent } from './public/components/footer/footer.component';
 import { LandingComponent } from './public/pages/landing/landing.component';
@@ -35,12 +31,7 @@ import { NotFoundComponent } from './public/pages/not-found/not-found.component'
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatButtonModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatSidenavModule,
-    MatFormFieldModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
