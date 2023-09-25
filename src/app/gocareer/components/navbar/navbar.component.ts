d581e7446cd659795442df5c76a6c4230650d6fd
Jack Arana <arack.dev@gmail.com>
2023-09-25 18:51:07 -0500

    feat(navbar): added navigation redirects for each section

--- src/app/gocareer/components/navbar/navbar.component.ts
+++ src/app/gocareer/components/navbar/navbar.component.ts
@@ -1,10 +1,29 @@
 import {Component, OnInit} from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent  {
+  constructor(private router: Router) { }
 
+  redirectToProfile() {
+    this.router.navigate(['/profile']); // Redirige al componente de inicio de sesión
+  }
+  redirectToNotifications() {
+    this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
+  }
+  redirectToSpecialists() {
+    this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
+  }
+  redirectToTest() {
+    this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
+  }
+  redirectToCareer() {
+    this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
+  }
+  signOut() {
+    this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
+  }
 }
