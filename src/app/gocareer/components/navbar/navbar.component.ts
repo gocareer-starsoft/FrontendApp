279e4e44fd1745a3a754dfe1a51fa37bf564ac2f
Jack Arana <arack.dev@gmail.com>
2023-09-25 18:57:45 -0500

    feat(navbar): add navbar v1

--- src/app/gocareer/components/navbar/navbar.component.ts
+++ src/app/gocareer/components/navbar/navbar.component.ts
@@ -9,19 +9,19 @@ export class NavbarComponent  {
   constructor(private router: Router) { }
 
   redirectToProfile() {
-    this.router.navigate(['/profile']); // Redirige al componente de inicio de sesión
+    this.router.navigate(['/profile']);
   }
   redirectToNotifications() {
-    this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
+    this.router.navigate(['/notifications']);
   }
   redirectToSpecialists() {
-    this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
+    this.router.navigate(['/notifications']);
   }
   redirectToTest() {
-    this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
+    this.router.navigate(['/notifications']);
   }
   redirectToCareer() {
-    this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
+    this.router.navigate(['/notifications']);
   }
   signOut() {
     this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
