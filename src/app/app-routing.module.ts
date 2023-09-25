3dfc9fe743c6c13b59cb81ed0c662c976b2a7dcb
Jack Arana <arack.dev@gmail.com>
2023-09-25 18:40:56 -0500

    feat(navbar): added navigation bar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LandingComponent} from "./public/pages/landing/landing.component";
 import { NotFoundComponent} from "./public/pages/not-found/not-found.component";
 import { LoginComponent} from "./public/components/login/login.component";
 import { ForgotpasswordComponent} from "./public/components/forgotpassword/forgotpassword.component";
+import { NavbarComponent} from "./gocareer/components/navbar/navbar.component";
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'checkpoint', component: ForgotpasswordComponent },
+  { path: 'navbar', component: NavbarComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
