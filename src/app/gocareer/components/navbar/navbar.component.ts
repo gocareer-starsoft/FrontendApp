aadf9d93213dd80904f62cec0ae23df29ff83b9c
Jack Arana <arack.dev@gmail.com>
2023-09-26 01:55:25 -0500

    feat(navbar): updated redirects

--- src/app/gocareer/components/navbar/navbar.component.ts
+++ src/app/gocareer/components/navbar/navbar.component.ts
@@ -8,21 +8,6 @@ import { Router } from '@angular/router';
 export class NavbarComponent  {
   constructor(private router: Router) { }
 
-  redirectToProfile() {
-    this.router.navigate(['/profile']);
-  }
-  redirectToNotifications() {
-    this.router.navigate(['/notifications']);
-  }
-  redirectToSpecialists() {
-    this.router.navigate(['/notifications']);
-  }
-  redirectToTest() {
-    this.router.navigate(['/notifications']);
-  }
-  redirectToCareer() {
-    this.router.navigate(['/notifications']);
-  }
   signOut() {
     this.router.navigate(['/notifications']); // Redirige al componente de inicio de sesión
   }
