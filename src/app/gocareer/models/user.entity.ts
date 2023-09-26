9c462044046f4f0d8873ca1ab9cddf8be75d259c
Jack Arana <arack.dev@gmail.com>
2023-09-26 01:58:06 -0500

    chore(gocareer): Create User Profile Model and Component

---
+++ src/app/gocareer/models/user.entity.ts
@@ -0,0 +1,14 @@
+export class User {
+    name: string;
+    email: string;
+    phone: string;
+    avatarUrl: string;
+    bio: string;
+    constructor() {
+        this.name = "name";
+        this.email = "email";
+        this.phone = "phone";
+        this.avatarUrl = "assets/images/home-test.jpg";
+        this.bio = "bio";
+    }
+}
--- src/app/gocareer/pages/profile/profile.component.ts
+++ src/app/gocareer/pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {User} from "../../models/user.entity";
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  user: User = new User();
 
+  userData = true;
+  editMode = false;
+  editData() {
+    this.editMode = true;
+    this.userData = false;
+  }
+  saveProfile() {
+    this.editMode = false;
+    this.userData = true;
+  }
 }
