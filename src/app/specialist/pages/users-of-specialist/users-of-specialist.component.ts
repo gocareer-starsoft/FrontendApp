547e2afb05e6ba08c0d91b121a2cf8c644da1df2
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2023-09-28 11:38:35 -0500

    feat(users-specialist): added specialist users page .

---
+++ src/app/specialist/pages/users-of-specialist/users-of-specialist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-of-specialist',
+  templateUrl: './users-of-specialist.component.html',
+  styleUrls: ['./users-of-specialist.component.css']
+})
+export class UsersOfSpecialistComponent {
+
+}
