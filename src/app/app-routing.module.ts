c1ba0b4fa7f6d20103f6634e94371340e83e3a44
Jack Arana <arack.dev@gmail.com>
2023-09-28 01:59:02 -0500

    chore(auth): Updated new routes and styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LandingComponent} from "./public/pages/landing/landing.component";
 import { NotFoundComponent} from "./public/pages/not-found/not-found.component";
 import { LoginComponent} from "./public/components/login/login.component";
 import { ForgotpasswordComponent} from "./public/components/forgotpassword/forgotpassword.component";
+import { NewuserComponent} from "./public/components/newuser/newuser.component";
 import { HomeComponent } from "./gocareer/pages/home/home.component";
 import { ProfileComponent } from "./gocareer/pages/profile/profile.component";
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: 'register', component: NewuserComponent },
   { path: 'login', component: LoginComponent },
   { path: 'checkpoint', component: ForgotpasswordComponent },
   { path: 'home', component: HomeComponent },
