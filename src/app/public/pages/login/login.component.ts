094139f5499da714089ce15abe7d5130d57e743d
Jack Arana <arack.dev@gmail.com>
2023-10-05 01:58:59 -0500

    feat(signup): updated styles

--- src/app/public/pages/login/login.component.ts
+++ src/app/public/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  isHidden: boolean = false;
+
+  constructor(private router: Router) {}
+  onSignup() {
+    this.isHidden = true;
+
+    setTimeout(() => {
+      this.router.navigate(['/signup']);
+    }, 500);
+  }
 }
--- src/app/public/pages/signup/signup.component.spec.ts
+++ src/app/public/pages/signup/signup.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NewuserComponent } from './newuser.component';
+import { SignupComponent } from './signup.component';
 
-describe('NewuserComponent', () => {
-  let component: NewuserComponent;
-  let fixture: ComponentFixture<NewuserComponent>;
+describe('SignupComponent', () => {
+  let component: SignupComponent;
+  let fixture: ComponentFixture<SignupComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NewuserComponent]
+      declarations: [SignupComponent]
     });
-    fixture = TestBed.createComponent(NewuserComponent);
+    fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
