492953cbdc1416898407896a6c4c57ba3c9cf34b
anonimat0 <andrealonsoarroyo@gmail.com>
2023-11-02 23:21:20 -0500

    feat(student): added page test-question

---
+++ src/app/student/pages/test-question/test-question.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-question',
+  templateUrl: './test-question.component.html',
+  styleUrls: ['./test-question.component.css']
+})
+export class TestQuestionComponent {
+
+}
