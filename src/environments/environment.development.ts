da7d33cff67d4e0ec4d2f421d13cdbebbdfe5889
Wilver Arana R <wilver.ar.dev@gmail.com>
2024-06-15 16:37:53 -0500

    feat: update - deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  serverBasePath: 'http://localhost:8080/api/v1'
+  serverBasePath: 'https://gocareer.azurewebsites.net/api/v1'
   //serverBasePath: 'https://gocareer.up.railway.app/api/v1'
 };
